6c7d8abb37b101f52f79d1502803c2a0ff9c8fd6
Evgeny Kravchenko <evgenyonechenko@gmail.com>
2026-07-10 21:56:45 +0600

    Initial commit

---
+++ src/main/java/ru/evgeny/asyncbookingsystem/AsyncBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.evgeny.asyncbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncBookingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncBookingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/evgeny/asyncbookingsystem/AsyncBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.evgeny.asyncbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
